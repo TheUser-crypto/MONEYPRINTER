b40a7caff5101a852b2a280b47eac72ff2dde3cb
Atatuber <atakan422005@gmail.com>
2025-03-04 23:41:51 +0100

    Standardized the X resulting in less errors and better weights

--- training.py
+++ training.py
@@ -4,45 +4,51 @@ import matplotlib.pyplot as plt
 
 
 # Not needed but for info :D
-def calculate_mse(y, y_hoc):
-    e = y - y_hoc
+def calculate_mse(y, y_hat):
+    e = y - y_hat
     return 0.5 * np.sum(e ** 2)
 
-# Number of students
-n = 100
 
-# Number of iterations
+def prepare_data(df):
+    X = df[['hours_studied', 'hours_slept']].values
+    y = df['score'].values.reshape(-1, 1)
+
+    # Standardization
+    X_normalized = (X - np.mean(X, axis=0)) / np.std(X, axis=0)
+    bias_column = np.ones((X_normalized.shape[0], 1))
+    X_with_bias = np.concatenate([bias_column, X_normalized], axis=1)
+
+    return X_with_bias, y
+
+
+def gradient_descent(epochs, X_with_bias, y, n):
+    mses = []
+    l_step = 0.01
+    w = np.random.randn(X.shape[1], 1)
+
+    for i in range(epochs):
+        y_hat = np.dot(X_with_bias, w)
+        mse = calculate_mse(y, y_hat)
+        mses.append(float(mse))
+        e = y - y_hat
+        g = -(1 / n) * np.dot(X_with_bias.T, e) 
+        w = w - l_step * g
+    return w, mses
+
+def plot_diagram(mses):
+    plt.plot(range(1, epochs + 1), mses, marker='o', markersize=3, linestyle='-')
+    plt.xlabel("Epoch")
+    plt.ylabel("Cost (MSE)")
+    plt.title("Cost Reduction over Epochs")
+    plt.grid(True)
+    plt.show()
+
 epochs = 1000
-
 df = pd.read_csv("datasets/sleep_casus.csv")
 
-l_step = 0.01
-
-X = df[['hours_studied', 'hours_slept']].values
-y = df['score'].values.reshape(-1, 1)
-
-w = np.array([[10], [5]])
-y_hoc = np.dot(X, w)
-
-mses = []
-
-for i in range(epochs):
-    y_hoc = np.dot(X, w)
-    mse = calculate_mse(y, y_hoc)
-    mses.append(float(mse))
-    e = y - y_hoc
-    g = -(1 / n) * np.dot(X.T, e) 
-    w = w - l_step * g
+X, y = prepare_data(df)
+n = X.shape[0]
+new_w, mses = gradient_descent(epochs, X, y, n)
 
 print(mses)
-
-print("New Weight: ", w)
-
-
-# Generate plot
-plt.plot(range(1, epochs + 1), mses, marker='o', markersize=3, linestyle='-')
-plt.xlabel("Epoch")
-plt.ylabel("Cost (MSE)")
-plt.title("Cost Reduction over Epochs")
-plt.grid(True)
-plt.show()
+plot_diagram(mses)
